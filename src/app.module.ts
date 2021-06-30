c1d5268d86740c949e03e7bc386dff6ccd58860b
Michael <guay.me@gmail.com>
2021-06-29 20:24:57 -0400

    Add GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     UsersModule,
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    DatabaseModule
+    MongooseModule.forRoot('mongodb://localhost/demo')
   ],
   controllers: [],
   providers: [],
